76cdb2906f614607182ca2be54a01ba054ac6052
kevin <you@example.com>
2024-06-10 12:15:03 +0300

    Updated Files

--- article.js
+++ article.js
@@ -1,3 +1,29 @@
+const ctrlVideo = document.querySelectorAll("#video");
+const button = document.querySelectorAll(".play-btn");
+
+button.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    video = btn.nextElementSibling;
+    ctrlVideo.forEach((vid) => {
+      if (!vid.paused) {
+        vid.pause();
+        vid.previousElementSibling.classList.remove("active");
+      }
+    });
+    video.play();
+    // video.requestFullscreen();
+    btn.classList.toggle("active");
+  });
+});
+
+ctrlVideo.forEach((vid) => {
+  vid.addEventListener("click", () => {
+    btns = vid.previousElementSibling;
+    btns.classList.remove("active");
+    vid.pause();
+  });
+});
+
 const step = document.querySelectorAll(".step");
 const line = document.querySelectorAll(".line");
 
